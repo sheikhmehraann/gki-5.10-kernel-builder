13748f055f191e9958ec3a2d69e0a912b8ba67a7
sheikhmehraann <mehraann.19@gmail.com>
2026-09-20 21:28:04 +0530

    fix: use clean anykernel_template.sh to fix workflow yaml indentation

---
+++ tools/anykernel_template.sh
@@ -0,0 +1,32 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# Global properties
+properties() { '
+kernel.string=Kernel By Mehraan (VARIANT)
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=
+supported.versions=5.10 - 5.10.270
+supported.patchlevels=
+'; } # end properties
+
+### AnyKernel install
+
+## Boot shell variables
+BLOCK=boot;
+IS_SLOT_DEVICE=auto;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# Import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+## Start boot install
+split_boot;
+flash_boot;
+## End boot install
